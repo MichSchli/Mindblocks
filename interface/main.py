08f62a4d87a71dd5a09a7405fff2d3a4998ff653
Michael Sejr Schlichtkrull <michael.sejr@gmail.com>
2016-11-16 15:18:25 +0100

    Added menu and tabs

--- interface/main.py
+++ interface/main.py
@@ -1,9 +1,12 @@
 import tkinter as tk
+from tkinter import ttk
 import imp
 
 network = imp.load_source('network','interface/network_canvas/network_canvas.py')
 optimizer = imp.load_source('optimizer','interface/optimizer_canvas/optimizer_canvas.py')
 dataset = imp.load_source('dataset','interface/dataset_canvas/dataset_canvas.py')
+
+menu = imp.load_source('menu','interface/other/menubar.py')
 
 
 
@@ -12,7 +15,18 @@ class Interface(tk.Tk):
         tk.Tk.__init__(self)
         self.add_canvases()
 
+        self.menubar = menu.Menubar(self)
+        self.config(menu=self.menubar)
+
     def add_canvases(self):
-        self.network_canvas = network.NetworkCanvas(self)
-        self.optimizer_canvas = optimizer.OptimizerCanvas(self)
-        self.dataset_canvas = dataset.DatasetCanvas(self)
+        self.note = ttk.Notebook(self)
+        
+        self.network_canvas = network.NetworkCanvas(self.note)
+        self.optimizer_canvas = optimizer.OptimizerCanvas(self.note)
+        self.dataset_canvas = dataset.DatasetCanvas(self.note)
+
+        self.note.add(self.network_canvas, text="Network")
+        self.note.add(self.optimizer_canvas, text="Optimizer")
+        self.note.add(self.dataset_canvas, text="Datasets")
+
+        self.note.pack()
---
+++ interface/other/menubar.py
@@ -0,0 +1,47 @@
+import tkinter as tk
+
+class Menubar(tk.Menu):
+
+    def get_menus(self):
+        return [
+            ("File",
+             [
+                 ("New", self.placeholder),
+                 ("Save", self.placeholder),
+                 ("Load", self.placeholder),
+                 ("Exit", self.quit_f)
+             ]
+            ),
+            ("Edit",
+             [
+                 ("Undo", self.placeholder),
+                 ("Redo", self.placeholder),
+                 ("Cut", self.placeholder),
+                 ("Copy", self.placeholder),
+                 ("Paste", self.placeholder)
+             ]
+            )
+        ]
+
+        
+    
+    def __init__(self, root):
+        tk.Menu.__init__(self, root)
+        self.root = root
+        
+        for menu in self.get_menus():
+            self.__add_menu_from_list__(*menu)
+
+    def placeholder(self):
+        pass
+            
+    def quit_f(self):
+        self.root.quit()
+            
+    def __add_menu_from_list__(self, title, l):
+        menu = tk.Menu(self, tearoff=0)
+        for k in l:
+            menu.add_command(label=k[0], command=k[1])
+
+        self.add_cascade(label=title, menu=menu)
+            
